4e7c531d1a3445af708a1852b179fb8ae60fe83f
Shashank Mishra <shashankpandit2018@gmail.com>
2023-08-06 18:55:17 +0530

    Mostly finished

--- styles/style.js
+++ styles/style.js
@@ -2,3 +2,22 @@ var typed = new Typed('#element', {
     strings: ['HTML', 'CSS','JavaScript','React','NodeJs','ExpressJs','MongoDb'],
     typeSpeed: 100,
   });
+
+  $(document).ready(function() {
+    // Add click event handler to images with the class "pic1" to "pic7"
+    $(".blog img").on("click", function() {
+      // Remove the "transform" class from all images first
+      $(".blog img").removeClass("transform");
+      
+      // Add the "transform" class to the clicked image
+      $(this).addClass("transform"); 
+      $(this).addClass("index");    
+   
+
+      setTimeout(() => {
+        $(this).removeClass("transform");
+      }, 5000);
+
+    });
+  });
+  
