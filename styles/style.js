c66ed38d35dee59e492d3b242f9f9f891fc38eee
ShashankMishra03 <shashankpandit2018@gmail.com>
2023-08-09 01:42:03 +0530

    added footer and some minr changes

--- styles/style.js
+++ styles/style.js
@@ -1,23 +1,29 @@
 var typed = new Typed('#element', {
-    strings: ['HTML', 'CSS','JavaScript','React','NodeJs','ExpressJs','MongoDb'],
-    typeSpeed: 100,
+  strings: ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJs', 'ExpressJs', 'MongoDb'],
+  typeSpeed: 100,
+});
+
+$(document).ready(function () {
+  // Add click event handler to images with the class "pic1" to "pic7"
+  $(".blog img").on("click", function () {
+    // Remove the "transform" class from all images first
+
+    // Add the "transform" class to the clicked image
+    $(this).toggleClass("transform");
+
+    // setTimeout(() => {
+    //   $(this).removeClass("transform");
+    // }, 5000);
+
+
+
   });
-
-  $(document).ready(function() {
-    // Add click event handler to images with the class "pic1" to "pic7"
-    $(".blog img").on("click", function() {
-      // Remove the "transform" class from all images first
-      $(".blog img").removeClass("transform");
-      
-      // Add the "transform" class to the clicked image
-      $(this).addClass("transform"); 
-      $(this).addClass("index");    
-   
-
-      setTimeout(() => {
-        $(this).removeClass("transform");
-      }, 5000);
-
-    });
+  $(".mid img").on("click", function () {
+    $(this).toggleClass("transfrm");
+    $(".mid p").text("")
+    setTimeout(() => {
+      $(".mid p").text("Shashank")
+    }, 5000);
   });
-  
+});
+
