d5b237c8ca4d7124b095781676ed5630dec07f79
ShashankMishra03 <shashankpandit2018@gmail.com>
2023-08-10 19:05:03 +0530

    changes in project look

--- styles/style.js
+++ styles/style.js
@@ -3,21 +3,5 @@ var typed = new Typed('#element', {
   typeSpeed: 100,
 });
 
-$(document).ready(function () {
-  // Add click event handler to images with the class "pic1" to "pic7"
-  $(".blog img").on("click", function () {
-    
 
-    // Toggle the "transform" class to the clicked image
-    $(this).toggleClass("transform");
-
-  });
-  $(".mid img").on("click", function () {
-    $(this).toggleClass("transfrm");
-    $(".mid p").text("")
-    setTimeout(() => {
-      $(".mid p").text("Shashank")
-    }, 5000);
-  });
-});
 
